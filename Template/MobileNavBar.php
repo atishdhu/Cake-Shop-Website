da54b91074ef883eb2b48225c89eeb69d66e8e0f
atishdhu <atish-dhunnoo@hotmail.com>
2020-12-17 12:40:28 +0400

    Add includes files in all pages

--- Template/MobileNavBar.php
+++ Template/MobileNavBar.php
@@ -12,11 +12,11 @@ echo '
         <h1 class="business-name"><a href="index.html" class="animate__animated animate__backInDown">Malako</a></h1>
 
         <ul>
-            <li><a class="active" href="index.php">HOME</a></li>
-            <li><a href="products.php">PRODUCTS</a></li>
-            <li><a href="makeyourcake.php">MAKE YOUR CAKE</a></li>
-            <li><a href="about.php">ABOUT</a></li>
-            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="index.php" class="<?php if($page == 'index'){echo 'active';}?>" href="index.php">HOME</a></li>
+            <li><a href="index.php" class="<?php if($page == 'products'){echo 'active';}?>" href="products.php">PRODUCTS</a></li>
+            <li><a href="index.php" class="<?php if($page == 'makeyourcake'){echo 'active';}?>" href="makeyourcake.php">MAKE YOUR CAKE</a></li>
+            <li><a href="index.php" class="<?php if($page == 'about'){echo 'active';}?>" href="about.php">ABOUT</a></li>
+            <li><a href="index.php" class="<?php if($page == 'contact'){echo 'active';}?>" href="contact.php">CONTACT US</a></li>
         </ul>
     </nav>
 </header>
